626234d9feb8302a7a144f81b132b08d7c53402a
spareaccount5555000 <130539488+spareaccount5555000@users.noreply.github.com>
2025-06-21 15:26:28 +0800

    Add files via upload

---
+++ Pggame1.py
@@ -0,0 +1,32 @@
+import pgzrun
+from random import randint
+
+WIDTH = 300
+HEIGHT = 300
+
+def draw():
+    r = 125
+    g = 0
+    b = randint(120,255)
+
+    width = WIDTH
+    height = HEIGHT - 200
+
+    for i in range(20):
+        rect = rect((0,0), (width, height))
+        rect.center = 150,150
+        screen.draw.rect(rect,(r,g,b))
+
+        r -= 10
+        g += 10
+
+        width -= 10
+        height += 10
+    
+pgzrun.go()
+
+
+
+
+
+
