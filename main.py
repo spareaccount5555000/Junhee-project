068506e75d9bbdb1ac7538686c6de5910108593f
spareaccount5555000 <130539488+spareaccount5555000@users.noreply.github.com>
2025-06-28 15:12:35 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,78 +1,32 @@
-#names = {
-   #"Hannah" : 23,
-   #"Johanna" : 21,
-   #"Lionel" : 22,
-   #"Jaycas" : 20
-#}
+import pgzrun
+from random import randint
 
-#print(names.values())
+TITLE = "Good Shot"
+WIDTH = 500
+HEIGHT = 500
 
+message = ""
 
-sample_dict = {
-    "name" : "junhee",
-    "age" : 12,
-    "city" : "Singapore"
-}
+alien = Actor('alien')
 
-print(sample_dict["name"])
+def draw():
+    screen.clear()
+    screen.fill(color = (204,229,255))
 
-print(sample_dict)
+    alien.draw()
+    screen.draw.text(message, center = (400,10), fontsize = 30)
 
-sample_list = ["Pulkit", 23 , "Agra"]
-print(sample_list[0])
+def place_alien():
+    alien.x = randint(50, WIDTH-50)
+    alien.y = randint(50, WIDTH-50)
 
-print(sample_dict.values())
+def on_mouse_down(pos):
+    global message
+    if alien.collidepoint(pos):
+        message = "good shot"
+        place_alien()
+    else:
+        message = "You missed"
 
-for key in sample_dict.keys():
-   print(key, sample_dict[key])
-
-if "name" in sample_dict:
-   print(sample_dict["name"])
-else:
-   print("key does not exist")
-
-
-sample_dict["profession"] = "Software Engineer"
-print(sample_dict)
-
-del(sample_dict["profession"])
-print(sample_dict)
-
-sample_dict["city"] = "Bangalore"
-print(sample_dict)
-
-sample_dict["marks"] = [99,87,85,92,90]
-print(sample_dict )
-
-x = sample_dict["marks"][1]
-print(x)
-
-classroom = {
-   "Jaycas" : {
-      "age" : 11,
-      "marks":[75,79,87,90,85]
-   }
-   ,"Alex" : {
-      "age" : 12,
-      "marks" : [90,96,84,79,88]
-   }
-}
-
-print(classroom.keys())
-
-print(classroom.values())
-
-for i in classroom.keys():
-   print(classroom[i])
-
-
-
-
-
-
-
-
-
-
-
-
+place_alien()
+pgzrun.go()
