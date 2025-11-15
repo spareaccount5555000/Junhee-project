eb4335555b51ed4c87d4c21893922ee89e61e91b
spareaccount5555000 <130539488+spareaccount5555000@users.noreply.github.com>
2025-11-15 15:46:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
-class Student():
-    register_num = 15
-    name = "alex"
-    studentsclass = 1
-    level = 6
-    grade = "A1"  
+import pgzrun
+import random
 
-    def __init__(self):
-        print("creating a new student")
+HEIGHT = 600
+WIDTH = 800
+GRAVITY = 2000
 
-    def changingdetails(self):
-        self.grade = input("what is this student's new grade:")
-        self.name = input("what is this student's new name:")
-        self.level = input("what is this student's new level:")
+class Ball:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vx = 50
+        self.vy = 0
+        self.r = 20
     
-    def displaydetails(self):
-        print(self.register_num)
-        print(self.name)
-        print(self.studentsclass)
-        print(self.level)
-        print(self.grade)
+    def draw(self):
+        pos = (self.x, self.y)
+        screen.draw.filled_circle( pos, self.r, "blue")
+
+ball1 = Ball(400, 300)
+
+def draw():
+    screen.clear()
+    ball1.draw()
+
+def update(dt):
+    uy = ball1.vy
+    ball1.vy += GRAVITY * dt
+    ball1.y += (uy + ball1.vy) * 0.5 * dt
+
+    if ball1.y > HEIGHT - ball1.r:
+        ball1.y = HEIGHT - ball1.r
+        ball1.vy = -ball1.vy * 0.5
+    
+    ball1.x += ball1.vx * dt
+
+    if ball1.x > WIDTH - ball1.r or ball1.x < ball1.r:
+        ball1.vx = -ball1.vx
+
+def on_key_down(key):
+    if key == keys.SPACE:
+        ball1.vy = -400
+
+pgzrun.go()
+
+
+
+
     
 
-ian = Student()
-ian.changingdetails()
-ian.displaydetails()
-    
 
 
 
-
-
+        
+        
