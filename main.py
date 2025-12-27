c37c2f0b83a39b30f04ce3146361abf91c141643
spareaccount5555000 <130539488+spareaccount5555000@users.noreply.github.com>
2025-12-27 16:00:32 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 import pygame
-import time
+from pygame.locals import *
+
 pygame.init()
+screen = pygame.display.set_mode((800, 600))
 
-x = 515
-y = 800
+rocket = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\rocketgame\\rocket.png")
+bg = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\rocketgame\\space.jpg")
 
-screen = pygame.display.set_mode((x, y))
+keys = [False, False, False, False]
 
-pygame.display.set_caption("Christmas card")
+rocketx = 400
+rockety = 300
 
-img1pre = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\christmascard\\christmastree.jpg")
-img1 = pygame.transform.scale(img1pre, (515, 800))
+while rockety < 600:
+    screen.blit(bg, (0, 0))
+    screen.blit(rocket, (rocketx, rockety))
+    pygame.display.flip()
 
-
-while True:
-    font = pygame.font.SysFont("TimesNewRoman", 24)
-    text = font.render("Wish you a merry Chirstmas", True, "black")
-    screen.fill("gray")
-    screen.blit(img1, (0, 0))
-    screen.blit(text, (0, 0))
-    pygame.display.update()
-    time.sleep(3)
-    img2pre = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\christmascard\\chirstmas2.jpg")
-    img2 = pygame.transform.scale(img2pre, (515, 800))
-    text2 = font.render("and a happy new year", True, "black")
-    screen.blit(img2, (0, 0))
-    screen.blit(text2, (0, 0))
-    pygame.display.update()
-    time.sleep(3)
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if i.type == pygame.KEYDOWN:
+            if i.key == K_UP:
+                keys[0] = True
+            if i.key == K_DOWN:
+                keys[1] = True
+            if i.key == K_LEFT:
+                keys[2] = True
+            if i.key == K_RIGHT:
+                keys[3] = True
+        if i.type == pygame.KEYUP:
+            if i.key == K_UP:
+                keys[0] = False
+            if i.key == K_DOWN:
+                keys[1] = False
+            if i.key == K_LEFT:
+                keys[2] = False
+            if i.key == K_RIGHT:
+                keys[3] = False
+    if keys[0]:
+        if rockety > 0:
+            rockety -= 0.3
+    if keys[1]:
+        if rockety < 600:
+            rockety += 0.3
+    if keys[2]:
+        if rocketx > 0:
+            rocketx -= 0.3
+    if keys[3]:
+        if rocketx < 800:
+            rocketx += 0.3
+    
+    rockety += 0.2
+    
+    
 
 
 
