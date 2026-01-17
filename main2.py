fc5e6e76bb3f52922c8f6163243014f70494226d
spareaccount5555000 <130539488+spareaccount5555000@users.noreply.github.com>
2026-01-17 15:16:03 +0800

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,30 @@
+import pygame
+import time
+pygame.init()
+
+x = 515
+y = 800
+
+screen = pygame.display.set_mode((x, y))
+
+pygame.display.set_caption("Christmas card")
+
+img1pre = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\christmascard\\birthday1.jpg")
+img1 = pygame.transform.scale(img1pre, (515, 800))
+
+
+while True:
+    font = pygame.font.SysFont("TimesNewRoman", 24)
+    text = font.render("Wish you a happy birthday", True, "black")
+    screen.fill("gray")
+    screen.blit(img1, (0, 0))
+    screen.blit(text, (0, 0))
+    pygame.display.update()
+    time.sleep(3)
+    img2pre = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\christmascard\\birthday2.jpg")
+    img2 = pygame.transform.scale(img2pre, (515, 800))
+    text2 = font.render("Happy birthday", True, "black")
+    screen.blit(img2, (0, 0))
+    screen.blit(text2, (0, 0))
+    pygame.display.update()
+    time.sleep(3)
