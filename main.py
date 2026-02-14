53706de850571487b42e81eb03348af982caedbb
spareaccount5555000 <130539488+spareaccount5555000@users.noreply.github.com>
2026-02-14 15:59:28 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,83 @@
 import pygame
+import random
+pygame.init()
 
-x = 800
-y = 600
+x = 864
+y = 936
+screen = pygame.display.set_mode((x,y))
 
-screen = pygame.display.set_mode((x, y))
-border = pygame.Rect(x//2 - 5, 0, 10, y)
-bg = pygame.image.load("spacebackground.png")
-rockety = pygame.image.load("rocketyellow.png")
-rockety1 = pygame.transform.rotate(pygame.transform.scale(rockety, (50, 40 )), -90)
-rocketr = pygame.image.load("rocketred.png")
-rocketr1 = pygame.transform.rotate(pygame.transform.scale(rocketr, (50, 40 )), 90)
-vel = 0.3
+bg = pygame.image.load("C:\Users\kiewj\Desktop\pro gd\flappybird\bg.png")
+ground = pygame.image.load("C:\Users\kiewj\Desktop\pro gd\flappybird\ground.png")
+pipe = pygame.image.load("C:\Users\kiewj\Desktop\pro gd\flappybird\pipe.png")
+restart = pygame.image.load("C:\Users\kiewj\Desktop\pro gd\flappybird\restart.png")
 
-def draw():
-    screen.blit(bg, (0, 0))
-    screen.blit(rocketr1, (150, 300))
-    screen.blit(rockety1, (650, 300))
-    pygame.display.update()
+ground_scroll = 0
+scroll_speed = 4
+flying = False
+game_over = False
+pipe_gap = 150
+pipe_frequency = 1500
+last_pipe = pygame.time.get_ticks() - pipe_frequency
+score = 0
+clock = pygame.time.Clock()
 
-def redmove(keys_press, red):
-    if keys_press[pygame.K_a] and red.x - vel > 0:
-        red.x -= vel
-    if keys_press[pygame.K_d] and red.x - vel < 400:
-        red.x += vel
-    if keys_press[pygame.K_w] and red.y - vel > 0:
-        red.y -= vel
-    if keys_press[pygame.K_s] and red.y - vel < 600:
-        red.y += vel
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for i in range(1,4):
+            img = pygame.image.load(f"C:\Users\kiewj\Desktop\pro gd\flappybird\bird{i}.png")
+            self.images.append(img)
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+        self.vel = 0
+        self.click = False
+    def update(self):
+        if flying == True:
+            self.vel += 0.5
+            if 8 < self.vel:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
+        if game_over == False:
+            if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
+                self.click = True
+                self.vel = -10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.click = False
+            flap_cooldown = 5
+            self.counter += 1
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+                self.image = self.images[self.index]
+            self.image = pygame.transform.rotate(self.images[self.index], self.vel * -2)
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index], -90)
 
-def yellowmove(keys_press, yellow):
-    if keys_press[pygame.K_LEFT] and yellow.x - vel > 400:
-        yellow.x -= vel
-    if keys_press[pygame.K_RIGHT] and yellow.x - vel < 800:
-        yellow.x += vel
-    if keys_press[pygame.K_UP] and yellow.y - vel > 0:
-        yellow.y -= vel
-    if keys_press[pygame.K_DOWN] and yellow.y - vel < 600:
-        yellow.y += vel
+bird_group = pygame.sprite.Group()
+flappy = Bird(100, int(y/2))
+bird_group.add(flappy)
 
-run = True
-red = pygame.Rect(150, 300, 50, 40)
-yellow = pygame.Rect(650, 300, 50, 40)
+while True:
+    clock.tick(60)
+    screen.blit(bg, (0,0))
+    bird_group.draw(screen)
+    bird_group.update()
+    screen.blit(ground, (ground_scroll, 768))
+    if flappy.rect.bottom >= 768:
+        game_over = True
+        flying = False
+        
 
-while run:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            run = False
-            pygame.quit()
-    draw()
-    keys_press = pygame.key.get_pressed()
-    redmove(keys_press, red)
-    yellowmove(keys_press, yellow)
+
+
+
+
+
+
