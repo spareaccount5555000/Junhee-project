baf4828b63dc95144205bb2c54ad4b1778e489a4
spareaccount5555000 <130539488+spareaccount5555000@users.noreply.github.com>
2026-04-04 14:51:53 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import random
 import time
 pygame.init()
 
-x = 900
-y = 700
+w = 900
+h = 700
 
-screen = pygame.display.set_mode((x, y))
+screen = pygame.display.set_mode((w, h))
 
 class Bin(pygame.sprite.Sprite):
     def __init__(self):
@@ -18,7 +18,7 @@ class Bin(pygame.sprite.Sprite):
 class Recycle(pygame.sprite.Sprite):
      def __init__(self, img):
         super().__init__()
-        self.image = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\recycle" + img).convert_alpha()
+        self.image = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\recycle\\" + img).convert_alpha()
         self.image = pygame.transform.scale(self.image, (30, 30))
         self.rect = self.image.get_rect()
 
@@ -30,8 +30,8 @@ class Nonrecycle(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
 
 def changebackground(img):
-    bg = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\recycle" + img)
-    bg = pygame.transform.scale(bg, (x, y))
+    bg1 = pygame.image.load("C:\\Users\\kiewj\\Desktop\\pro gd\\recycle\\" + img)
+    bg = pygame.transform.scale(bg1, (w, h))
     screen.blit(bg, (0, 0))
 
 images = ["item1.png", "box.png", "pencil.png"]
@@ -41,15 +41,15 @@ plastic = pygame.sprite.Group()
 
 for i in range(50):
     item = Recycle(random.choice(images))
-    item.rect.x = random.randint(0, x)
-    item.rect.y = random.randint(0, y)
+    item.rect.x = random.randint(0, w)
+    item.rect.y = random.randint(0, h)
     item_list.add(item)
     all_sprites.add(item)
 
 for i in range(20):
     item = Nonrecycle()
-    item.rect.x = random.randint(0, x)
-    item.rect.y = random.randint(0, y)
+    item.rect.x = random.randint(0, w)
+    item.rect.y = random.randint(0, h)
     plastic.add(item)
     all_sprites.add(item)
 
@@ -62,7 +62,54 @@ clock = pygame.time.Clock()
 start_time = time.time()
 font = pygame.font.SysFont("Arial", 18)
 pygame.font.init()
-text = font.render(f"Score: {}".format(score), True, "Black")
+text = font.render("Score: " + str(score),True, "Black")
+
+while run:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    time_elapsed = time.time() - start_time
+    if time_elapsed >= 60 or score >= 20:
+        if score >= 20:
+            text = font.render("You Win, Well Done!", True, "Black")
+            changebackground("youwin.jpg")
+        else:
+            text = font.render("You lost, Try again", True, "Black")
+            changebackground("youlose.jpg")
+        screen.blit(text, (150, 100))
+    else:
+        changebackground("background.png")
+        countdown = font.render("Time left:" + str(60 - int(time_elapsed)), True, "Black")
+        screen.blit(countdown, (10, 10))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            if bin.rect.y > 0:
+                bin.rect.y -= 5
+        if keys[pygame.K_DOWN]:
+            if bin.rect.y < h:
+                bin.rect.y += 5
+        if keys[pygame.K_LEFT]:
+            if bin.rect.x > 0:
+                bin.rect.x -= 5
+        if keys[pygame.K_RIGHT]:
+            if bin.rect.x < w:
+                bin.rect.x += 5
+        item_hit = pygame.sprite.spritecollide(bin, item_list, True)
+        plastic_hit = pygame.sprite.spritecollide(bin, plastic, True)
+        for i in item_hit:
+            score += 1
+            text = font.render("Score: " + str(score), True, "Black")
+        for i in plastic_hit:
+            score -= 5
+            text = font.render("Score: " + str(score), True, "Black")
+        screen.blit(text, (10, 50))
+        all_sprites.draw(screen)
+    pygame.display.update()
+
+
+
+
 
 
 
